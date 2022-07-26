d549825d159787e0f67e754c823055c783bc27a9
shukeel <triggaillest@gmail.com>
2022-07-26 22:47:49 +0300

    modified

--- src/App.js
+++ src/App.js
@@ -1,60 +1,8 @@
-import './App.css'
-import { useState } from "react"
-import Form from "./components/Form"
-import Header from "./components/Header"
-import List from './components/List'
+import React from 'react'
 
 const App = () => {
-
-    const [input, setInput] = useState('');
-    const [todo, setTodo] = useState([]);
-
-    const handleInputChange = (e) => {
-        setInput(e.target.value);
-
-    };
-
-    const addTask = () => {
-        const task = {
-            id : todo.length === 0 ? 1: todo[todo.length - 1].id + 1,
-            taskName: input,
-            completed: false,
-        };
-
-        setTodo([...todo, task]);
-    };
-
-    const completeTask = (id) => {
-        setTodo(todo.map((task) => task.id === id ? {...task, completed: true} : task))
-
-    };
-
-
-    const deleteTask = (id) => {
-        setTodo(todo.filter(task => task.id !== id));
-    };
-    
   return (
-        <div className='App'>
-            <div className='main'>
-                <div className='header-section'>
-                    <Header />
-                </div>
-                <div>
-                    <div>
-                        <Form handleInputChange={handleInputChange} addTask={addTask} />
-                    </div>                
-                </div>
-                <div>
-                    {todo.map((task) => {
-                        return (
-                            <List taskName={task.taskName} id={task.id} completed={task.completed} deleteTask={deleteTask} completedTask={completeTask} />
-                        )
-                    })}
-                </div>
-            </div>
-
-      </div>
+    <div>App</div>
   )
 }
 
